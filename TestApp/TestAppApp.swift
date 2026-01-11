9fee16eaa6fb6894417fe2777121bbd66be28a9a
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2026-01-11 11:17:53 +0200

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Vitalie Bancu on 26.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
