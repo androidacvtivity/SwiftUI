9914bc847047e04810b4abaecf537e1dbb0eddfc
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2026-01-13 08:30:23 +0200

    Text modifications

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("bancusoft")
+            Text("bancusoft - 2026")
         }
         .padding()
     }
